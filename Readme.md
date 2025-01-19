24cc7b624dd4c50006e900fcc7b979c3f10ef8d5
muhammadkashif20 <kashif28@gmail.com>
2025-01-19 18:24:02 +0500

    Working On Different Types Of Middleware

--- Readme.md
+++ Readme.md
@@ -1,2 +1,8 @@
 # Complete-Backend-Development
  This Repo is My All (Backend Development) Working Like (MongoDB , Node & Express JS)
+<!-- Class 1 Create Your First Api With Node & Express Js
+1.req and res differcene
+2.req behjo ge to response ayega res bhejna lazmi h.
+--> 
+<!-- Class 2 Create REST APIs (POST, GET, PUT, DELETE) and Use Postman.  -->
+<!--Class 3 What is Middleware in Node.js Express? A Complete Guide -->
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
 const PORT = 4000;
 const tasks = [
@@ -8,21 +9,31 @@ const tasks = [
     { id: 4, task: "Go for a walk" },
     { id: 5, task: "Read a book" }
   ];
-  
+// Application Level Middleware
+  const middleware=(req,res,next)=>{
+    console.log("app level middleware=> ",Date.now());
+    next()
+  }
+app.use(middleware)
+// The End Application Level Middleware
+// show send req name middleware:-
+app.use(morgan('tiny'))
+// The End show send req name middleware:-
+
 app.get("/", (req, res) => {
-  console.log(req);
+  // console.log(req);
   res.send(tasks);
 });
 app.post("/", (req, res) => {
-  console.log(req);
+  // console.log(req);
   res.send("Hello World To First Api POST");
 });
 app.put("/", (req, res) => {
-  console.log(req);
+  // console.log(req);
   res.send("Hello World To First Api PUT");
 });
 app.delete("/", (req, res) => {
-  console.log(req);
+  // console.log(req);
   res.send("Hello World To First Api DELETE");
 });
 app.listen(PORT, () => console.log("Server Is Running on PORT=> ", PORT));
