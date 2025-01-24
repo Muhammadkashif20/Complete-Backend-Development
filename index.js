5c09e08b65a11c376ce92d37da7c2acec50bab0f
muhammadkashif20 <kashif28@gmail.com>
2025-01-24 15:56:50 +0500

    Working on Params & Query & Body

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import userRouter from "./routers/users.js"
 const app = express();
 const PORT = 4000;
 const tasks = [
-  { id: 1, task: "Buy groceries" },
-  { id: 2, task: "Complete React assignment" },
-  { id: 3, task: "Call the electrician" },
-  { id: 4, task: "Go for a walk" },
-  { id: 5, task: "Read a book" },
+  { id: 1, task: "Buy groceries", complete:true },
+  { id: 2, task: "Complete React assignment" , complete:true},
+  { id: 3, task: "Call the electrician" , complete:false},
+  { id: 4, task: "Go for a walk" , complete:false},
+  { id: 5, task: "Read a book", complete:true },
 ];
 const middleware = (req, res, next) => {
   // console.log("app level middleware=> ",Date.now());
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   // console.log("req.requestBy=> ", req.requestBy);
   // console.log("res.responseBy=> ", res.responseBy);
   // console.log("req.body=> ", req.body);
-  res.send(tasks);
+  res.status(200).send(tasks);
 });
 
 // app.post("/", middleware, (req, res) => {
@@ -39,5 +39,17 @@ app.get("/", (req, res) => {
 // app.delete("/", (req, res) => {
 //   res.send("Hello World To First Api DELETE");
 // });
+// params  dynamic route and url without params api is not completed.
+app.get("/singleTask/:id",(req,res)=>{
+  const findTask=tasks.find((data)=>data.id == req.params.id)
+  if(!findTask) return res.status(404).send("Task Not Found")
+    res.status(200).send(findTask)
+})
+// query
+
+
 app.listen(PORT, () => console.log("Server Is Running on PORT=> ", PORT));
 
+// params 
+// query
+// body
